5e63aa51095f8fa5c6d57ef52d9c2560de8099f2
Koushik <vkkumar2002@gmail.com>
2025-11-23 00:00:58 +0530

    About us Removed

--- App.tsx
+++ App.tsx
@@ -8,26 +8,26 @@ import Geofence from './components/Geofence';
 import SwapStations from './components/SwapStations';
 import Rentals from './components/Rentals';
 import Contact from './components/Contact';
-import AboutUs from './components/AboutUs';
+// import AboutUs from './components/AboutUs';
 import Footer from './components/Footer';
 import ScrollToTopButton from './components/ScrollToTopButton';
 
 export default function App() {
-  return (
-    <div className="bg-walor-dark">
-      <Header />
-      <main>
-        <Hero />
-        <Features />
-        <HowItWorks />
-        <Geofence />
-        <SwapStations />
-        <Rentals />
-        <Contact />
-        <AboutUs />
-      </main>
-      <Footer />
-      <ScrollToTopButton />
-    </div>
-  )
+    return (
+        <div className="bg-walor-dark">
+            <Header />
+            <main>
+                <Hero />
+                <Features />
+                <HowItWorks />
+                <Geofence />
+                <SwapStations />
+                <Rentals />
+                <Contact />
+                {/* <AboutUs /> */}
+            </main>
+            <Footer />
+            <ScrollToTopButton />
+        </div>
+    )
 }
